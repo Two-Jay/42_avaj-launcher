58310ecb733d58e88097bb8fa5d1060eec5f99c4
Two-Jay <djeeee1272@gmail.com>
2022-12-25 01:57:21 +0900

    Aircraft 클래스에서 getMessageByWeather 구현

--- kr/ft/avaj/simulator/Aircraft.java
+++ kr/ft/avaj/simulator/Aircraft.java
@@ -4,6 +4,7 @@ public class Aircraft {
     protected long id;
     protected String name;
     protected Coordinates coordinates;
+    protected String type;
     private static long idCounter = 0;
 
     protected Aircraft(String name, Coordinates coordinates) {
@@ -14,5 +15,28 @@ public class Aircraft {
 
     private long nextId() {
         return ++idCounter;
+    }
+
+    protected String getByWeather(String weather) {
+        switch (weather) {
+            case "RAIN":
+                return "It's raining. Better watch out for lightings.";
+            case "FOG":
+                return "It's foggy. Be careful not to crash.";
+            case "SUN":
+                return "It's sunny. Let's enjoy the good weather and take some pics.";
+            case "SNOW":
+                return "It's snowing. We're gonna crash.";
+            default:
+                return "Unknown weather.";
+        }
+    }
+
+    private String buildSpeakMessage(String message) {
+        return "";
+    }
+
+    public void speak(String message) {
+        System.out.println(buildSpeakMessage(message));
     }
 }

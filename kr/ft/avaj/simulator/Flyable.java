38ffcaf0d64755ee0d962d534d87dcb82e86f836
Two-Jay <djeeee1272@gmail.com>
2022-12-25 02:08:44 +0900

    Flyable 에 speak 함수를 추상함수로 추가

--- kr/ft/avaj/simulator/Flyable.java
+++ kr/ft/avaj/simulator/Flyable.java
@@ -3,4 +3,5 @@ package kr.ft.avaj.simulator;
 public interface Flyable {
     public abstract void updateConditions();
     // public abstract void registerTower(WeatherTower weatherTower);
+    public abstract void speak();
 }

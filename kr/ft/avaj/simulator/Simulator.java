5c3340c404545d11ab1de58397b838edf7db46b0
Two-Jay <djeeee1272@gmail.com>
2022-12-24 14:20:04 +0900

    Scanner 를 이용한 readFile 메소드 추가

--- kr/ft/avaj/simulator/Simulator.java
+++ kr/ft/avaj/simulator/Simulator.java
@@ -1,9 +1,34 @@
 package kr.ft.avaj.simulator;
 
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.io.File;
+import java.io.FileNotFoundException;
 
 public class Simulator {
+    private static ArrayList<String> readFile(String filepath) throws FileNotFoundException {
+        File file = new File(filepath);
+        Scanner scanner = new Scanner(file);
+        System.err.println("Reading file: " + filepath);
+        ArrayList<String> lines = new ArrayList<String>();
+
+        while (scanner.hasNextLine()) {
+            lines.add(scanner.nextLine());
+        }
+        scanner.close();
+
+        for (String line : lines) {
+            System.err.println(line);
+        }
+        return lines;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        try {
+            ArrayList<String> lines = readFile(args[0]);
+        } catch (FileNotFoundException e) {
+            System.err.println("File not found: " + args[0]);
+            System.exit(1);
+        }
     }
 };

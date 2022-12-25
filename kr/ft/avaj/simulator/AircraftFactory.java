e4c0bf26f03d281d3742b8b2f9d53508abf7a123
Two-Jay <djeeee1272@gmail.com>
2022-12-26 02:58:06 +0900

    사용자 정의 Exception에 메세지 포멧 추가

--- kr/ft/avaj/simulator/AircraftFactory.java
+++ kr/ft/avaj/simulator/AircraftFactory.java
@@ -8,10 +8,10 @@ public class AircraftFactory {
     public Flyable newAircraft(String type, String name, int longitude, int latitude, int height) {
         try {
             if (longitude < 0 || latitude < 0 || height < 0) {
-                throw new InvalidCoordinatesException();
+                throw new InvalidCoordinatesException(longitude, latitude, height);
             }
             if (!aircraftTypes.contains(type)) {
-                throw new InvalidAircraftTypeException();
+                throw new InvalidAircraftTypeException(type);
             }
             Coordinates coordinates = new Coordinates(longitude, latitude, height);
             if (type.equals("Baloon")) {
@@ -22,10 +22,10 @@ public class AircraftFactory {
                 return new Helicopter(name, coordinates);
             }
         } catch (InvalidCoordinatesException e) {
-            System.out.println("Invalid coordinates");
+            System.out.println(e.getMessage());
             System.exit(1);
         } catch (InvalidAircraftTypeException e) {
-            System.out.println("Invalid aircraft type");
+            System.out.println(e.getMessage());
             System.exit(1);
         }
         return null;
--- kr/ft/avaj/simulator/InvalidAircraftTypeException.java
+++ kr/ft/avaj/simulator/InvalidAircraftTypeException.java
@@ -1,4 +1,11 @@
 package kr.ft.avaj.simulator;
 
 class InvalidAircraftTypeException extends RuntimeException {
+    public InvalidAircraftTypeException(String aircraftName) {
+        super(buildExceptionMessage(aircraftName));
+    }
+
+    private static String buildExceptionMessage(String aircraftName) {
+        return "Exception : invalid coordinates. (" + aircraftName + ")";
+    }
 }

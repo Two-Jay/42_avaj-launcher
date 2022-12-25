3388daab89e2ddce978ed18798a0b8d167a22c0b
Two-Jay <djeeee1272@gmail.com>
2022-12-26 01:44:50 +0900

    Tower 클래스 registerFlyable 실행시 메세지 출력하도록 구현

--- kr/ft/avaj/simulator/Tower.java
+++ kr/ft/avaj/simulator/Tower.java
@@ -4,13 +4,20 @@ import java.util.ArrayList;
 
 public class Tower {
     private ArrayList<Flyable> observers = new ArrayList<Flyable>();
+    Printer printer = Printer.getInstance();
 
     public void register(Flyable flyable) {
+        printer.printToFile(buildRegisterMessage(flyable));
         this.observers.add(flyable);
     }
 
     public void unregister(Flyable flyable) {
         this.observers.remove(flyable);
+    }
+
+    private String buildRegisterMessage (Flyable flyable) {
+        String message = "Tower says: " + flyable.buildAircraftBarcode() + " registered to weather tower.";
+        return message;
     }
 
     protected void conditionsChanged() {

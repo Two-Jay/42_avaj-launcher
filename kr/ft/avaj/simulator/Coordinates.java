d0aa30adcceb2e4159f661ff54ae02b80f44e131
Two-Jay <djeeee1272@gmail.com>
2022-12-26 03:23:44 +0900

    Coordinates 클래스에 setter 함수 추가

--- kr/ft/avaj/simulator/Coordinates.java
+++ kr/ft/avaj/simulator/Coordinates.java
@@ -11,6 +11,23 @@ public class Coordinates {
         this.height = height;
     }
 
+    public void setLongitude(int longitude) {
+        this.longitude = longitude;
+    }
+
+    public void setLatitude(int latitude) {
+        this.latitude = latitude;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+        if (this.height > 100) {
+            this.height = 100;
+        } else if (this.height < 0) {
+            this.height = 0;
+        }
+    }
+
     public int getLongitude() {
         return this.longitude;
     }

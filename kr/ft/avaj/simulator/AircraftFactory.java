45730af8e01f50f4fec7c5710b86e64f2c98a887
Two-Jay <djeeee1272@gmail.com>
2022-12-26 02:41:25 +0900

    사용자 정의 Exception 클래스 AircraftFactory에 적용

--- kr/ft/avaj/simulator/AircraftFactory.java
+++ kr/ft/avaj/simulator/AircraftFactory.java
@@ -1,18 +1,39 @@
 package kr.ft.avaj.simulator;
 
+import java.util.ArrayList;
+
 public class AircraftFactory {
+    private ArrayList<String> aircraftTypes = new ArrayList<String>();
+
     public Flyable newAircraft(String type, String name, int longitude, int latitude, int height) {
-        Coordinates coordinates = new Coordinates(longitude, latitude, height);
-        if (type.equals("Baloon")) {
-            return new Baloon(name, coordinates);
-        } else if (type.equals("JetPlane")) {
-            return new JetPlane(name, coordinates);
-        } else if (type.equals("Helicopter")) {
-            return new Helicopter(name, coordinates);
-        } else {
-            System.err.println("Invalid aircraft type: " + type);
+        try {
+            if (longitude < 0 || latitude < 0 || height < 0) {
+                throw new InvalidCoordinatesException();
+            }
+            if (!aircraftTypes.contains(type)) {
+                throw new InvalidAircraftTypeException();
+            }
+            Coordinates coordinates = new Coordinates(longitude, latitude, height);
+            if (type.equals("Baloon")) {
+                return new Baloon(name, coordinates);
+            } else if (type.equals("JetPlane")) {
+                return new JetPlane(name, coordinates);
+            } else if (type.equals("Helicopter")) {
+                return new Helicopter(name, coordinates);
+            }
+        } catch (InvalidCoordinatesException e) {
+            System.out.println("Invalid coordinates");
+            System.exit(1);
+        } catch (InvalidAircraftTypeException e) {
+            System.out.println("Invalid aircraft type");
             System.exit(1);
         }
         return null;
+    }
+
+    AircraftFactory() {
+        aircraftTypes.add("Baloon");
+        aircraftTypes.add("JetPlane");
+        aircraftTypes.add("Helicopter");
     }
 }

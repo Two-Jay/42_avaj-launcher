56676164bcfcee27614ff031f92c0392fa80bd45
Two-Jay <djeeee1272@gmail.com>
2023-04-24 01:20:03 +0900

    Simulation 객체 멤버변수항목 업데이트

--- kr/ft/avaj/simulator/Simulator/Simulator.java
+++ kr/ft/avaj/simulator/Simulator/Simulator.java
@@ -4,42 +4,38 @@ import java.util.ArrayList;
 
 import kr.ft.avaj.simulator.Aircraft.Flyable;
 import kr.ft.avaj.simulator.Aircraft.Coordinates.Coordinates;
-import kr.ft.avaj.simulator.Aircraft.Factory.AircraftFactory;
 import kr.ft.avaj.simulator.Parser.*;
 import kr.ft.avaj.simulator.Utils.*;
 import kr.ft.avaj.simulator.WeatherTower.WeatherTower;
 
 public class Simulator {
-    SimulationMetaData simulationMetaData = new SimulationMetaData();
-    ParserInterface parser;
-    AircraftFactory aircraftFactory;
-    int simulationCount;
-    ArrayList<Flyable> aircrafts;
+    private SimulationMetaData metaData = new SimulationMetaData();
+    private ParserInterface parser;
+    private ParsedData parsedData;
 
     public Simulator(String filePath) {
         parser = new Parser(filePath);
-        aircraftFactory = new AircraftFactory();
-
-        ParsedData parsedData = parser.getParsedData();
-        this.simulationCount = parsedData.getSimulationCount();
-        this.aircrafts = parsedData.getAircrafts();
-        System.out.println("Simulation count: " + parsedData.getSimulationCount());
-        System.out.println("Aircrafts: " + aircrafts.size());
+        parsedData = parser.getParsedData();
     }
 
     private Boolean isNeededToDetach(Flyable aircraft) {
         Coordinates current = aircraft.getCoordinates();
 
-        return current.getHeight() <= simulationMetaData.heightMinimum
-                || current.getLatitude() < simulationMetaData.latitudeMinimum
-                || current.getLongitude() < simulationMetaData.longitudeMinimum
-                || current.getLongitude() >= simulationMetaData.longitudeMaximum
-                || current.getLatitude() >= simulationMetaData.latitudeMaximum;
+        return current.getHeight() <= metaData.heightMinimum
+                || current.getLatitude() < metaData.latitudeMinimum
+                || current.getLongitude() < metaData.longitudeMinimum
+                || current.getLongitude() >= metaData.longitudeMaximum
+                || current.getLatitude() >= metaData.latitudeMaximum;
     }
 
     public void run() {
         Printer.getInstance().setFilePath("simulation.txt");
         WeatherTower weatherTower = new WeatherTower();
+        int simulationCount = parsedData.getSimulationCount();
+        ArrayList<Flyable> aircrafts = parsedData.getAircrafts();
+
+        System.out.println("Simulation count: " + parsedData.getSimulationCount());
+        System.out.println("Aircrafts: " + aircrafts.size());
 
         for (Flyable aircraft : aircrafts) {
             aircraft.registerTower(weatherTower);

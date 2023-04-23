21faf89e2402424f4b30b163f254ac301c161309
Two-Jay <djeeee1272@gmail.com>
2023-04-24 02:15:59 +0900

    접근제한자 수정, 네이밍 수정

--- kr/ft/avaj/simulator/WeatherProvider/Field.java
+++ kr/ft/avaj/simulator/WeatherProvider/Field.java
@@ -4,9 +4,9 @@ import java.util.ArrayList;
 import kr.ft.avaj.simulator.Aircraft.Coordinates.Coordinates;
 
 public class Field {
-    long height;
-    long width;
-    ArrayList<ArrayList<String>> FieldInfo = new ArrayList<ArrayList<String>>();
+    private long height;
+    private long width;
+    private ArrayList<ArrayList<String>> fieldInfo = new ArrayList<ArrayList<String>>();
 
     Field(long height, long width, String[] weather) {
         this.height = height;
@@ -16,13 +16,13 @@ public class Field {
     }
 
     public void updateWeather(String[] weather) {
-        this.FieldInfo.clear();
+        this.fieldInfo.clear();
         for (int i = 0; i < height; i++) {
             ArrayList<String> row = new ArrayList<String>();
             for (int j = 0; j < width; j++) {
                 row.add(generateRandomWeather(weather));
             }
-            this.FieldInfo.add(row);
+            this.fieldInfo.add(row);
         }
     }
 
@@ -32,10 +32,10 @@ public class Field {
     }
 
     public String getWeather(int x, int y) {
-        return FieldInfo.get(x).get(y);
+        return fieldInfo.get(x).get(y);
     }
 
     public String getWeather(Coordinates coordinates) {
-        return FieldInfo.get(coordinates.getLongitude()).get(coordinates.getLatitude());
+        return fieldInfo.get(coordinates.getLongitude()).get(coordinates.getLatitude());
     }
 }

708b29fc93745af3293b156f1fbe624dd64b7731
Two-Jay <djeeee1272@gmail.com>
2023-04-24 10:25:53 +0900

    updateStrategy 내에서 인터페이스 의존성 정리

--- kr/ft/avaj/simulator/Aircraft/UpdateStrategy/AircraftMoveUpdateStrategy.java
+++ kr/ft/avaj/simulator/Aircraft/UpdateStrategy/AircraftMoveUpdateStrategy.java
@@ -2,7 +2,7 @@ package kr.ft.avaj.simulator.Aircraft.UpdateStrategy;
 
 import kr.ft.avaj.simulator.Aircraft.Coordinates.Coordinates;
 
-public abstract class AircraftMoveUpdateStrategy {
+public abstract class AircraftMoveUpdateStrategy implements AircraftMoveUpdateStrategyInterface {
     public void addToCoordinates(Coordinates coordinates, int addToLongitude, int addToLatitude, int addToHeight) {
         coordinates.setLongitude(coordinates.getLongitude() + addToLongitude);
         coordinates.setLatitude(coordinates.getLatitude() + addToLatitude);

7f211319373e1ac93ecb33b6fb96caf41cce071c
Two-Jay <djeeee1272@gmail.com>
2023-07-24 19:12:59 +0900

    Parser 메소드 나누기

--- kr/ft/avaj/simulator/Parser/ParserImpl.java
+++ kr/ft/avaj/simulator/Parser/ParserImpl.java
@@ -21,7 +21,8 @@ public class ParserImpl implements Parser {
 
     public ParsedDataDTO parseFile(String filePath) throws Exception {
         Scanner sc = new Scanner(new File(filePath));
-        ParsedDataDTO parsedDataDTO = new ParsedDataDTO(this.parseSimulationCount(sc), this.parseAircrafts(sc));
+        ParsedDataDTO parsedDataDTO = new ParsedDataDTO(this.parseSimulationCount(sc),
+                this.parseAircraftInformations(sc));
         return parsedDataDTO;
     }
 
@@ -32,21 +33,25 @@ public class ParserImpl implements Parser {
         return Integer.parseInt(tokens[SimulationMetaData.getInstance().simulationCountIndex]);
     }
 
-    private ArrayList<AircraftInfo> parseAircrafts(Scanner sc) throws Exception {
+    private ArrayList<AircraftInfo> parseAircraftInformations(Scanner sc) throws Exception {
         ArrayList<AircraftInfo> aircrafts = new ArrayList<AircraftInfo>();
         while (sc.hasNextLine()) {
             String[] tokens = sc.nextLine().split(" ");
-            if (tokens.length != 5)
-                throw new Exception("Invalid aircraft data");
-            AircraftInfo aircraftInfo = new AircraftInfo.Builder()
-                    .setType(tokens[0])
-                    .setName(tokens[1])
-                    .setLongitude(Integer.parseInt(tokens[2]))
-                    .setLatitude(Integer.parseInt(tokens[3]))
-                    .setHeight(Integer.parseInt(tokens[4]))
-                    .build();
-            aircrafts.add(aircraftInfo);
+            aircrafts.add(initAircraftInfo(tokens));
         }
         return aircrafts;
+    }
+
+    private AircraftInfo initAircraftInfo(String[] tokens) throws Exception {
+        if (tokens.length != 5)
+            throw new Exception("Invalid aircraft data");
+        AircraftInfo aircraftInfo = new AircraftInfo.Builder()
+                .setType(tokens[0])
+                .setName(tokens[1])
+                .setLongitude(Integer.parseInt(tokens[2]))
+                .setLatitude(Integer.parseInt(tokens[3]))
+                .setHeight(Integer.parseInt(tokens[4]))
+                .build();
+        return aircraftInfo;
     }
 }
